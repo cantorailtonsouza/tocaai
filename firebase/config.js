e8b330dff2964e5646cc348e017f92a43e04039f
cantorailtonsouza <cantorailtonsouza@gmail.com>
2026-08-04 09:18:22 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,10 +1,13 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCpXPnrifffphHH6z6hoP8hMAMWs5wDN57Q",
-  authDomain: "tocaai ailtonsouza".replace(" ", "") + ".firebaseapp.com",
-  databaseURL: "https://tocaai ailtonsouza-default-rtdb.firebaseio.com".replace(" ", ""),
-  projectId: "tocaai ailtonsouza".replace(" ", ""),
-  storageBucket: "tocaai ailtonsouza.firebasestorage.app".replace(" ", ""),
+  apiKey: "AIzaSyCpXPnriffphHH6z6hoP8hMAMWs5wDN57Q",
+  authDomain: "tocaaiailtonsouza.firebaseapp.com",
+  databaseURL:
+    "https://tocaaiailtonsouza-default-rtdb.firebaseio.com",
+  projectId: "tocaaiailtonsouza",
+  storageBucket:
+    "tocaaiailtonsouza.firebasestorage.app",
   messagingSenderId: "653274837750",
-  appId: "1:653274837750:web:081cea9c54cc494d5572d7",
+  appId:
+    "1:653274837750:web:081cea9c54cc494d5572d7",
   measurementId: "G-SEM4TKG5FX"
 };
